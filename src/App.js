84161b53a2f5b531ad59504d2e5bf21200fecd9b
ka0824 <rkgud0824@gmail.com>
2022-04-04 23:31:08 +0900

    feat: practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increase, decrease } from './modules/counter'
+
 
 function App() {
+  const number = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{`현재 숫자는 ${number}입니다.`}</div>
+      <button onClick={() => dispatch(increase())}>증가</button>
+      <button onClick={() => dispatch(decrease())}>감소</button>
     </div>
   );
 }
